ddd3e89acfbe74934cf92dfe3d06e53fe160a0dd
alejanby207 <alejanby207@gmail.com>
2025-06-03 17:23:34 -0600

    Reemplazo carreras-asignaturas-microservicio por degree-program-microservice

---
+++ degree-program-microservice/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CourseModule } from './course/course.module';
+import { CurriculumModule } from './curriculum/curriculum.module';
+
+@Module({
+  imports: [CourseModule, CurriculumModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
