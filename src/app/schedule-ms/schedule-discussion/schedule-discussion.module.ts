f701fcf13983ccf703bb403636b94bb4066475b8
Victor Gabriel Lagos Bulnes <victor.lagos@ujcv.edu.hn>
2025-11-07 12:19:15 -0600

    Preparacion de ENV

--- src/app/schedule-ms/schedule-discussion/schedule-discussion.module.ts
+++ src/app/schedule-ms/schedule-discussion/schedule-discussion.module.ts
@@ -3,11 +3,12 @@ import { ScheduleDiscussionService } from './schedule-discussion.service';
 import { ScheduleDiscussionController } from './schedule-discussion.controller';
 import { PrismaModule } from 'src/common/prisma/prisma.module';
 import { AcademicPeriodModule } from 'src/app/academic-period/academic-period.module';
+import { ScheduleModule } from '../schedule/schedule.module';
 
 @Module({
-  imports: [PrismaModule, AcademicPeriodModule ],
+  imports: [PrismaModule, AcademicPeriodModule, ScheduleModule],
   controllers: [ScheduleDiscussionController],
   providers: [ScheduleDiscussionService],
-  exports:[ScheduleDiscussionService],
+  exports: [ScheduleDiscussionService],
 })
 export class ScheduleDiscussionModule { }
